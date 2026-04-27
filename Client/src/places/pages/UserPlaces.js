044e545bc53ccc0c217173cce2501635d4e98b89
Radha Krishna <radhakrishnauppugandla@gmail.com>
2026-04-27 15:50:32 -0400

    initial commit-SnapScape

---
+++ Client/src/places/pages/UserPlaces.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire state",
+    description: "One of most mosting state",
+    imageURL: "",
+    address: "707 MLK Drive W",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire state",
+    description: "One of most mosting state",
+    imageURL: "",
+    address: "727 MLK Drive W",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: "u2",
+  },
+];
+
+const UserPlaces = () => {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
+};
+
+export default UserPlaces;
